96ceee4553faaf49b950d502eb9eefacf6e92913
Matt <mnoxon@techego.com>
2016-02-23 16:12:37 -0700

    Second commit, fixing some thangs and adding some content.

--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -2,6 +2,20 @@ angular.module('align')
   .controller('mainCtrl', function(mainService){
     var main = this;
 
+    main.doctors = [
+      {
+        name: 'Dr. Walker',
+        title: 'The Dopest',
+        img: './img/walker.jpg'
+      },
+      {
+        name: 'Dr. Clifford',
+        title: 'The Dopestest',
+        img: './img/clifford.jpg'
+      }
+
+    ]
+
     main.yup = 'aww yiss'
     console.log(main.yup)
   })
--- js/services/mainService.js
+++ js/services/mainService.js
@@ -1,4 +1,4 @@
 angular.module('align')
   .service('mainService', function(){
-    
+    var key = 'AIzaSyD8dA8cHjIoZnKWujvaCBL1JMaofYcys2k';
   })
