f2f2044ab50e7946cf113c67de8f5cb1d210b76f
Matt <mnoxon@techego.com>
2016-03-04 09:48:23 -0700

    Pushing minor styling changes

--- js/controllers/bioCtrl.js
+++ js/controllers/bioCtrl.js
@@ -2,4 +2,9 @@ app.controller('bioCtrl', function($scope,$stateParams,mainService) {
   var name = $stateParams.shortName;
   $scope.currentBio = mainService.getBio(name)
 
+    $scope.showModal = false;
+    $scope.open = function(){
+    $scope.showModal = !$scope.showModal;
+    };
+
 })
--- js/controllers/mainCtrl.js
+++ js/controllers/mainCtrl.js
@@ -1,7 +1,6 @@
 angular.module('align')
   .controller('mainCtrl', function(mainService){
     var main = this;
-
     main.doctors = mainService.getDoctors();
 
     main.pages = [
@@ -9,32 +8,35 @@ angular.module('align')
       {
         id: 1,
         link: '#/home',
+        name: 'home',
         text: 'Home'
       },
       {
         id: 2,
         link: '#/staff',
+        name: 'staff',
         text: 'Staff'
       },
       {
         id: 3,
         link: '#/mission',
+        name: 'mission',
         text: 'Mission'
       },
       {
         id: 4,
         link: '#/model',
+        name: 'model',
         text: 'Model'
       },
       {
         id: 5,
         link: '#/contact',
+        name: 'contact',
         text: 'Contact'
       }
 
     ]
-
-    main.selectedIndex = 0;
 
     main.setActive = function(page) {
       main.selectedIndex = page;
